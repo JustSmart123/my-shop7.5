3e16536f7b72ca603c2b284bb873744da60d1bfd
Liliya Sviatenko <123566371+Liliya-ivanovna@users.noreply.github.com>
2025-02-11 14:22:58 +0200

    update

---
+++ my-shop7.5/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <ProductList />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    ProductList,
+    FooterComponent,
+  },
+};
+</script>
